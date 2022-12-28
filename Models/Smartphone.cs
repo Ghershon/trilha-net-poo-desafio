10870e702f6a287c3ad87b5e2fad9dd7d1f858ec
Gerson <95258058+Ghershon@users.noreply.github.com>
2022-12-27 20:12:40 -0400

    Add files via upload

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; }
+        private int Imei { get; }
+        private int Memoria { get; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, int imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -19,6 +23,11 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+
+        public void MostrarNumero()
+        {
+            Console.WriteLine(this.Numero);
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
